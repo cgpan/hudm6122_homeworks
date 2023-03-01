c5a26b6c2550e36999e8a8c68f34f68d2821e159
cgpan <grepan3280@gmail.com>
2023-03-01 17:13:51 -0500

    Ex 4.2 Finished

--- homework_04/test.R
+++ homework_04/test.R
@@ -1,7 +1,49 @@
-a <- matrix(0,5,5)
-dim(a)[2]
+library("MVA")
+demo("Ch-MDS")
 
-a[1,1] <- 1
-a
+chi_squared_dist_matrices <- function(tbl){
+  # Calculate row sums and column sums
+  row_sums <- apply(tbl, 1, sum)
+  col_sums <- apply(tbl, 2, sum)
+  # Calculate total count and expected counts
+  total_count <- sum(tbl)
+  
+  # to have the dim of the input matrix
+  c <- ncol(tbl)
+  r <- nrow(tbl)
+  # make a empty matrix to load all elements
+  col_d_matrix <- matrix(0,c,c)
+  
+  # using loop to write the each ij entries into this matrix
+  for (i in c(1:c)) {
+    for (j in c(1:c)) {
+      d_ij <- 0
+      for (k in c(1:r)) {
+        p_k_dot <- row_sums[k]/total_count
+        p_k_i <- tbl[k,i]/col_sums[i]
+        p_k_j <- tbl[k,j]/col_sums[j]
+        d_ij <- d_ij + (1/p_k_dot)*(p_k_i-p_k_j)^2
+      }
+      col_d_matrix[i,j] <- d_ij
+    }
+  }
+  # make a empty matrix to load all elements
+  row_d_matrix <- matrix(0,r,r)
+  
+  # using loop to write the each ij entries into this matrix
+  for (i in c(1:r)) {
+    for (j in c(1:r)) {
+      d_ij <- 0
+      for (k in c(1:r)) {
+        p_dot_k <- col_sums[k]/total_count
+        p_i_k <- tbl[i,k]/row_sums[i]
+        p_j_k <- tbl[j,k]/row_sums[j]
+        d_ij <- d_ij + (1/p_dot_k)*(p_i_k-p_j_k)^2
+      }
+      row_d_matrix[i,j] <- d_ij
+    }
+  }
+  return(list(col_d_matrix, row_d_matrix))
+}
 
-sample(c(1:200),10, replace = T)
+chi_squared_dist_matrices(tbl)
