95bc32dc785aa841b7332b597205f60e8a3ef716
cgpan <grepan3280@gmail.com>
2023-03-08 11:27:45 -0500

    Update Midterm_Review.R

--- Midterm_Review.R
+++ Midterm_Review.R
@@ -65,3 +65,47 @@ r12 <- LAdepr[1:2, -(1:2)]
 r21 <- LAdepr[-(1:2), 1:2]
 r12
 r21
+E1 <- solve(r11)%*%r12%*%solve(r22)%*%r21
+E2 <- solve(r22)%*%r21%*%solve(r11)%*%r12
+
+
+# -------------------------------------------------------------
+x <- rnorm(5)
+y <- rnorm(5)
+
+# calculate the distance
+sum((x-y)^2)
+
+# norm function will be helpful.
+?norm()
+
+# make sure the input is matrix! R always give a number array df
+x <- matrix(x,5,1)
+y <- matrix(y,5,1)
+# calculate the euclidean distance
+norm(x-y, type = "F")^2 
+# we need to square to have the elu-distance, since norm "F" gives you d_ij
+# not d_ij^2
+
+# -------------------- class 7 ----------------------
+n <- 20
+q <- 3
+X <- matrix(rnorm(n*q),n,q)
+S <- t(X)%*%X
+B <- X %*% t(X)
+# to have the rank of the matrix of S and B using rankMatrix
+library(Matrix)
+rankMatrix(X)
+
+round(eigen(S)$value, 5)
+dim(S)
+
+# we have 20 eignvalues and eigenvectors, but we only have 3!!rank!!
+round(eigen(B)$value, 5)
+dim(B)
+
+lambda <- round(eigen(B)$values, 5)
+V <- eigen(B)$vectors
+dim(V)
+lambda <- eigen(B)$values
+B2 <-lambda[1]*V[,1]*t(V[,1])+lambda[2]*V[,2]*t(V[,2])+lambda[3]*V[,3]*t(V[,3])
